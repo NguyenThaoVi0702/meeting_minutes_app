9c895f38aae377baf649a28e8cba0cfb566b2fda
NguyenThaoVi0702 <105551975+NguyenThaoVi0702@users.noreply.github.com>
2025-09-15 11:13:21 +0700

    Update and rename result.py to result_test.txt

--- result.py
+++
@@ -1,2 +0,0 @@
-
-1e3e2c76d79c   qdrant/qdrant:latest                                                "./entrypoint.sh"        2 months ago     Up 2 months                     0.0.0.0:7333->6333/tcp, [::]:7333->6333/tcp, 0.0.0.0:7334->6334/tcp, [::]:7334->6334/tcp   qdrant_container_test
